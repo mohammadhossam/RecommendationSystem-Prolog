2c8e71331e24fd754daed95bf1facebb26f09f2a
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2020-03-17 23:57:41 +0200

    Commit

--- Project.pl
+++ Project.pl
@@ -39,13 +39,15 @@ possibleSubset(L, R):-
    subSet(L, R1),
    permutation(R1, R).
 
+possibleActivity([H|T], Acc, R):-
+	H \== activity(X),
+	possibleActivity(T, [H|Acc] , R).
+
 possibleActivity([activity(X)|T], Acc, R):-
 	possibleSubset(X, R1),
 	reverse(Acc, RAcc),
 	append(RAcc, [activity(R1)], L1),
 	append(L1, T, R).
-possibleActivity([H|T], Acc, R):-
-	possibleActivity(T, [H|Acc], R).
 
 choosePreferences(Prefs , ChosenPreferences):-
     possibleSubset(Prefs , ChosenPreferences),
@@ -55,4 +57,45 @@ choosePreferences(Prefs , ChosenPreferences):-
     possibleSubset(Prefs , ChosenPreferences1),
 	member(activity(X), ChosenPreferences1),
 	possibleActivity(ChosenPreferences1, [], ChosenPreferences).
+
+activitiesRate([],_,0).
+activitiesRate([H|T],C,S):-
+	customerPreferredActivity(C,H,R),
+	activitiesRate(T,C,R1),
+	S is R+R1.
+
+preferenceSatisfaction(O, Customer, ChosenPrefs, S):-
+	O=offer(Destination, Activities, Cost, ValidFrom, ValidTo, Period, Duration, NoOfGuests),
+
+	(member(dest(Destination),ChosenPrefs);
+	\+member(dest(X), ChosenPrefs)),
+
+	((member(activity(Activities),ChosenPrefs)
+	 ,activitiesRate(Activities,Customer,S1));
+	(\+member(activity(Activities),ChosenPrefs),
+	 S1 is 0)),
+
+	((member(budget(X),ChosenPrefs), X>=Cost );
+	(\+member(budget(X),ChosenPrefs))),
+
+	((member(means(M),ChosenPrefs),
+	offerMean(O,M),
+	customerPreferredMean(Customer,M,S2)));
+	(\+member(means(M),ChosenPrefs),
+	S2 is 0),
+
+	((member(accommodation(A),ChosenPrefs),
+	offerAccommodation(O,A),
+	customerPreferredAccommodation(Customer,A,S3)));
+	(\+member(accommodation(A),ChosenPrefs),
+	S3 is 0),
+
+	S is S1+S2+S3.
+
+before(Y1-M1-D1 , Y2-M2-D2):-
+	(Y1 =< Y2);(Y1 = Y2 , M2 >= M1);(Y1 = Y2 , M2 = M1 ,D2 >= D1).
+
+overlapPeriod(period(Y11-M11-D11 ,Y12-M12-D12) , period(Y21-M21-D21 , Y22-M22-D22)):-
+(before(Y11-M11-D11,Y21-M21-D21) , before(Y21-M21-D21 , Y12-M12-D12));
+(before(Y11-M11-D11 , Y22-M22-D22) , before(Y22-M22-D22 , Y12-M12-D12)).
 
